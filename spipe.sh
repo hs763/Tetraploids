742cc9347e7a6c31081ba5a57a62166365711f12
Hanna Szafranska <109690308+hs763@users.noreply.github.com>
2024-08-14 12:18:14 +0100

    Update spipe.sh
    Runign each sublibrary though spipe individually and htne combining the ouputs using spipe - combine

--- spipe.sh
+++ spipe.sh
@@ -14,37 +14,45 @@ split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpe
 --fq2 $path2data/expdata/UDI-WT-1_R2_001.fastq.gz \
 --output_dir $path2data/analysis/UDI_WT_1
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
+nohup split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
 --fq1 $path2data/expdata/UDI-WT-2_R1_001.fastq.gz \
 --fq2 $path2data/expdata/UDI-WT-2_R2_001.fastq.gz \
---output_dir $path2data/analysis/UDI_WT_2
+--output_dir $path2data/analysis/UDI_WT_2 &
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
+nohup  split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
 --fq1 $path2data/expdata/UDI-WT-3_R1_001.fastq.gz \
 --fq2 $path2data/expdata/UDI-WT-3_R2_001.fastq.gz \
---output_dir $path2data/analysis/UDI_WT_3
+--output_dir $path2data/analysis/UDI_WT_3 &
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
+nohup split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
 --fq1 $path2data/expdata/UDI-WT-4_R1_001.fastq.gz \
 --fq2 $path2data/expdata/UDI-WT-4_R2_001.fastq.gz \
---output_dir $path2data/analysis/UDI_WT_4
+--output_dir $path2data/analysis/UDI_WT_4 &
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
+nohup split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
 --fq1 $path2data/expdata/UDI-WT-5_R1_001.fastq.gz \
 --fq2 $path2data/expdata/UDI-WT-5_R2_001.fastq.gz \
---output_dir $path2data/analysis/UDI_WT_5
+--output_dir $path2data/analysis/UDI_WT_5 &
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
+nohup split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
 --fq1 $path2data/expdata/UDI-WT-6_R1_001.fastq.gz \
 --fq2 $path2data/expdata/UDI-WT-6_R2_001.fastq.gz \
---output_dir $path2data/analysis/UDI_WT_6
+--output_dir $path2data/analysis/UDI_WT_6 &
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
+nohup split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
 --fq1 $path2data/expdata/UDI-WT-7_R1_001.fastq.gz \
 --fq2 $path2data/expdata/UDI-WT-7_R2_001.fastq.gz \
---output_dir $path2data/analysis/UDI_WT_7
+--output_dir $path2data/analysis/UDI_WT_7 &
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
+nohup split-pipe --mode all --kit WT --chemistry v2 --genome_dir /data1/hania/MultiSpeciesComp/newvolume/genomes/GRCh38/ \
 --fq1 $path2data/expdata/UDI-WT-8_R1_001.fastq.gz \
 --fq2 $path2data/expdata/UDI-WT-8_R2_001.fastq.gz \
---output_dir $path2data/analysis/UDI_WT_8
+--output_dir $path2data/analysis/UDI_WT_8 &
+
+path2sublibs="/data1/hania/tetraploids/newvolume/analysis"
+split-pipe \
+    --mode comb \
+    --sublibraries $path2sublib/UDI_WT_1 $path2sublib/UDI_WT_2 $path2sublib/UDI_WT_3 $path2sublib/UDI_WT_4 $path2sublib/UDI_WT_5 $path2sublib/UDI_WT_6 $path2sublib/UDI_WT_7 $path2sublib/UDI_WT_8 \
+    --output_dir $path2sublib/combined
+
+    
