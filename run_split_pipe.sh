a6771b47c12277f920119113f82ab74bd24df9ba
Hanna Szafranska <109690308+hs763@users.noreply.github.com>
2024-08-16 12:35:22 +0100

    Update run_split_pipe.sh

--- run_split_pipe.sh
+++ run_split_pipe.sh
@@ -1,7 +1,6 @@
 #script saved on /cephfs2/hannas/tetraploids/analysis/run_split_pipe.sh
 #make executable: chmod +x run_split_pipe.sh
 
-
 #!/bin/bash
 
 # Activate the conda environment
@@ -17,48 +16,48 @@ split-pipe --mode dge --kit WT --chemistry v2 \
     --fq1 $path2data/expdata/UDI-WT-1_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-1_2.fastq.gz \
     --output_dir $path2data/analysis/UDI_WT_1
-
+#each of the following individually: 
 split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
     --fq1 $path2data/expdata/UDI-WT-2_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-2_2.fastq.gz \
     --output_dir $path2data/analysis/UDI_WT_2
 
-nohup split-pipe --mode dge --kit WT --chemistry v2 \
+split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
     --fq1 $path2data/expdata/UDI-WT-3_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-3_2.fastq.gz \
-    --output_dir $path2data/analysis/UDI_WT_3 &
+    --output_dir $path2data/analysis/UDI_WT_3 
 
-nohup split-pipe --mode dge --kit WT --chemistry v2 \
+split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
     --fq1 $path2data/expdata/UDI-WT-4_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-4_2.fastq.gz \
-    --output_dir $path2data/analysis/UDI_WT_4 &
+    --output_dir $path2data/analysis/UDI_WT_4 
 
-nohup split-pipe --mode dge --kit WT --chemistry v2 \
+split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
     --fq1 $path2data/expdata/UDI-WT-5_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-5_2.fastq.gz \
-    --output_dir $path2data/analysis/UDI_WT_5 &
+    --output_dir $path2data/analysis/UDI_WT_5 
 
-nohup split-pipe --mode dge --kit WT --chemistry v2 \
+split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
     --fq1 $path2data/expdata/UDI-WT-6_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-6_2.fastq.gz \
-    --output_dir $path2data/analysis/UDI_WT_6 &
+    --output_dir $path2data/analysis/UDI_WT_6 
 
-nohup split-pipe --mode dge --kit WT --chemistry v2 \
+split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
     --fq1 $path2data/expdata/UDI-WT-7_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-7_2.fastq.gz \
-    --output_dir $path2data/analysis/UDI_WT_7 &
+    --output_dir $path2data/analysis/UDI_WT_7 
 
-nohup split-pipe --mode dge --kit WT --chemistry v2 \
+split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
     --fq1 $path2data/expdata/UDI-WT-8_1.fastq.gz \
     --fq2 $path2data/expdata/UDI-WT-8_2.fastq.gz \
-    --output_dir $path2data/analysis/UDI_WT_8 &
+    --output_dir $path2data/analysis/UDI_WT_8 
 
 split-pipe --mode comb \
     --sublibraries $path2data/analysis/UDI_WT_1 $path2data/analysis/UDI_WT_2 $path2data/analysis/UDI_WT_3 $path2data/analysis/UDI_WT_4 $path2data/analysis/UDI_WT_5 $path2data/analysis/UDI_WT_6 $path2data/analysis/UDI_WT_7 $path2data/analysis/UDI_WT_8 \
