b9b3d218cc3d0813f106170f97df5ec65896b900
Hanna Szafranska <109690308+hs763@users.noreply.github.com>
2024-08-30 12:24:46 +0100

    Update run_split_pipe.sh

--- run_split_pipe.sh
+++ run_split_pipe.sh
@@ -11,54 +11,144 @@ conda activate spipe
 path2data="/cephfs2/hannas/tetraploids"
 
 # Run split-pipe commands
+split-pipe --mode all --kit WT --chemistry v2 \
+    --genome_dir $path2data/genomes/GRCh38/ \
+    --fq1 $path2data/expdata/raw_data/UDI-WT-1_1.fastq.gz \
+    --fq2 $path2data/expdata/raw_data/UDI-WT-1_2.fastq.gz \
+    --output_dir $path2data/expdata/data2/UDI_WT_1
+    --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+# Activate the conda environment
+source /lmb/home/hannas/miniconda3/etc/profile.d/conda.sh
+conda activate spipe
+
+# Set path to data
+path2data="/cephfs2/hannas/tetraploids"
+
+#each of the following individually: 
+split-pipe --mode all --kit WT --chemistry v2 \
+     --genome_dir $path2data/genomes/GRCh38/ \
+     --fq1 $path2data/expdata/raw_data/UDI-WT-2_1.fastq.gz \
+     --fq2 $path2data/expdata/raw_data/UDI-WT-2_2.fastq.gz \
+     --output_dir $path2data/expdata/data2/UDI_WT_2
+     --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+# Activate the conda environment
+source /lmb/home/hannas/miniconda3/etc/profile.d/conda.sh
+conda activate spipe
+
+# Set path to data
+path2data="/cephfs2/hannas/tetraploids"
+
+split-pipe --mode all --kit WT --chemistry v2 \
+    --genome_dir $path2data/genomes/GRCh38/ \
+    --fq1 $path2data/expdata/raw_data/UDI-WT-3_1.fastq.gz \
+    --fq2 $path2data/expdata/raw_data/UDI-WT-3_2.fastq.gz \
+    --output_dir $path2data/expdata/data2/UDI_WT_3 
+    --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+# Activate the conda environment
+source /lmb/home/hannas/miniconda3/etc/profile.d/conda.sh
+conda activate spipe
+
+# Set path to data
+path2data="/cephfs2/hannas/tetraploids"
+
+split-pipe --mode all --kit WT --chemistry v2 \
+    --genome_dir $path2data/genomes/GRCh38/ \
+    --fq1 $path2data/expdata/raw_data/UDI-WT-4_1.fastq.gz \
+    --fq2 $path2data/expdata/raw_data/UDI-WT-4_2.fastq.gz \
+    --output_dir $path2data/expdata/data2/UDI_WT_4 
+    --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+# Activate the conda environment
+source /lmb/home/hannas/miniconda3/etc/profile.d/conda.sh
+conda activate spipe
+
+# Set path to data
+path2data="/cephfs2/hannas/tetraploids"
+
+split-pipe --mode all --kit WT --chemistry v2 \
+    --genome_dir $path2data/genomes/GRCh38/ \
+    --fq1 $path2data/expdata/raw_data/UDI-WT-5_1.fastq.gz \
+    --fq2 $path2data/expdata/raw_data/UDI-WT-5_2.fastq.gz \
+    --output_dir $path2data/expdata/data2/UDI_WT_5 
+    --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+# Activate the conda environment
+source /lmb/home/hannas/miniconda3/etc/profile.d/conda.sh
+conda activate spipe
+
+# Set path to data
+path2data="/cephfs2/hannas/tetraploids"
+
+split-pipe --mode all --kit WT --chemistry v2 \
+    --genome_dir $path2data/genomes/GRCh38/ \
+    --fq1 $path2data/expdata/raw_data/UDI-WT-6_1.fastq.gz \
+    --fq2 $path2data/expdata/raw_data/UDI-WT-6_2.fastq.gz \
+    --output_dir $path2data/expdata/data2/UDI_WT_6 
+    --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+# Activate the conda environment
+source /lmb/home/hannas/miniconda3/etc/profile.d/conda.sh
+conda activate spipe
+
+# Set path to data
+path2data="/cephfs2/hannas/tetraploids"
+
+split-pipe --mode all --kit WT --chemistry v2 \
+    --genome_dir $path2data/genomes/GRCh38/ \
+    --fq1 $path2data/expdata/raw_data/UDI-WT-7_1.fastq.gz \
+    --fq2 $path2data/expdata/raw_data/UDI-WT-7_2.fastq.gz \
+    --output_dir $path2data/expdata/data2/UDI_WT_7 
+    --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+# Activate the conda environment
+source /lmb/home/hannas/miniconda3/etc/profile.d/conda.sh
+conda activate spipe
+
+# Set path to data
+path2data="/cephfs2/hannas/tetraploids"
+
 split-pipe --mode dge --kit WT --chemistry v2 \
     --genome_dir $path2data/genomes/GRCh38/ \
-    --fq1 $path2data/expdata/UDI-WT-1_1.fastq.gz \
-    --fq2 $path2data/expdata/UDI-WT-1_2.fastq.gz \
-    --output_dir $path2data/analysis/UDI_WT_1
+    --fq1 $path2data/expdata/raw_data/UDI-WT-8_1.fastq.gz \
+    --fq2 $path2data/expdata/raw_data/UDI-WT-8_2.fastq.gz \
+    --output_dir $path2data/expdata/data2/UDI_WT_8 
+    --samp_sltab $path2data/expdata/raw_data/Parse_WT_100K_Sample_Loading_Table_V1.3.0.xlsm
 
-# #each of the following individually: 
-# split-pipe --mode dge --kit WT --chemistry v2 \
-#     --genome_dir $path2data/genomes/GRCh38/ \
-#     --fq1 $path2data/expdata/UDI-WT-2_1.fastq.gz \
-#     --fq2 $path2data/expdata/UDI-WT-2_2.fastq.gz \
-#     --output_dir $path2data/analysis/UDI_WT_2
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+sbatch -J UDI_WT_1 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_1.sh
+sbatch -J UDI_WT_2 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_2.sh
+sbatch -J UDI_WT_3 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_3.sh
+sbatch -J UDI_WT_4 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_4.sh
+sbatch -J UDI_WT_5 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_5.sh
+sbatch -J UDI_WT_6 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_6.sh
+sbatch -J UDI_WT_7 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_7.sh
+sbatch -J UDI_WT_8 -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G UDI_WT_8.sh
 
-# split-pipe --mode dge --kit WT --chemistry v2 \
-#     --genome_dir $path2data/genomes/GRCh38/ \
-#     --fq1 $path2data/expdata/UDI-WT-3_1.fastq.gz \
-#     --fq2 $path2data/expdata/UDI-WT-3_2.fastq.gz \
-#     --output_dir $path2data/analysis/UDI_WT_3 
-
-# split-pipe --mode dge --kit WT --chemistry v2 \
-#     --genome_dir $path2data/genomes/GRCh38/ \
-#     --fq1 $path2data/expdata/UDI-WT-4_1.fastq.gz \
-#     --fq2 $path2data/expdata/UDI-WT-4_2.fastq.gz \
-#     --output_dir $path2data/analysis/UDI_WT_4 
-
-# split-pipe --mode dge --kit WT --chemistry v2 \
-#     --genome_dir $path2data/genomes/GRCh38/ \
-#     --fq1 $path2data/expdata/UDI-WT-5_1.fastq.gz \
-#     --fq2 $path2data/expdata/UDI-WT-5_2.fastq.gz \
-#     --output_dir $path2data/analysis/UDI_WT_5 
-
-# split-pipe --mode dge --kit WT --chemistry v2 \
-#     --genome_dir $path2data/genomes/GRCh38/ \
-#     --fq1 $path2data/expdata/UDI-WT-6_1.fastq.gz \
-#     --fq2 $path2data/expdata/UDI-WT-6_2.fastq.gz \
-#     --output_dir $path2data/analysis/UDI_WT_6 
-
-# split-pipe --mode dge --kit WT --chemistry v2 \
-#     --genome_dir $path2data/genomes/GRCh38/ \
-#     --fq1 $path2data/expdata/UDI-WT-7_1.fastq.gz \
-#     --fq2 $path2data/expdata/UDI-WT-7_2.fastq.gz \
-#     --output_dir $path2data/analysis/UDI_WT_7 
-
-# split-pipe --mode dge --kit WT --chemistry v2 \
-#     --genome_dir $path2data/genomes/GRCh38/ \
-#     --fq1 $path2data/expdata/UDI-WT-8_1.fastq.gz \
-#     --fq2 $path2data/expdata/UDI-WT-8_2.fastq.gz \
-#     --output_dir $path2data/analysis/UDI_WT_8 
 
 #each fo these are in seperate files. 
 
@@ -88,4 +178,5 @@ split-pipe --mode comb \
     --output_dir $path2data/analysis/combined
 
 sbatch -J combine -c 2 --mail-type=ALL --mail-user=hannas@mrc-lmb.cam.ac.uk --mem=300G combine.sh
+
 
